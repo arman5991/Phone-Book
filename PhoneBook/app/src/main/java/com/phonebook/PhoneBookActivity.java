c7674af66c495850b345a2f891e4b9b4e98c64d8
Arman <arman5991@mai.ru>
2017-11-20 12:07:07 +0400

    Phone Book

---
+++ PhoneBook/app/src/main/java/com/phonebook/PhoneBookActivity.java
@@ -0,0 +1,45 @@
+package com.phonebook;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class PhoneBookActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.search);
+                    return true;
+                case R.id.navigation_my_profil:
+                    mTextMessage.setText(R.string.my_profile);
+                    return true;
+                case R.id.navigation_vip:
+                    mTextMessage.setText(R.string.vip);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_book);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
