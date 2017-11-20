22def3ab9daeec921da9f259682331a59f8a2a30
Arman <arman5991@mai.ru>
2017-11-20 17:42:56 +0400

    toolBar

--- PhoneBook/app/src/main/java/com/phonebook/PhoneBookActivity.java
+++ PhoneBook/app/src/main/java/com/phonebook/PhoneBookActivity.java
@@ -1,15 +1,24 @@
 package com.phonebook;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class PhoneBookActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private boolean b = true;
+    private Toolbar toolBar;
+    private SearchView searchView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,18 +27,20 @@ public class PhoneBookActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.search);
-                    return true;
+                    toolBar.setVisibility(View.VISIBLE);
+                    replaceFragment(SearchFragment.newInstance());
+                    return false;
                 case R.id.navigation_my_profil:
-                    mTextMessage.setText(R.string.my_profile);
-                    return true;
+                    toolBar.setVisibility(View.GONE);
+                    replaceFragment(MyProfileFragment.newInstance());
+                    return false;
                 case R.id.navigation_vip:
-                    mTextMessage.setText(R.string.vip);
-                    return true;
+                    toolBar.setVisibility(View.GONE);
+                    replaceFragment(VIPFragment.newInstance());
+                    return false;
             }
             return false;
         }
-
     };
 
     @Override
@@ -37,9 +48,33 @@ public class PhoneBookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_book);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+         toolBar = (Toolbar) findViewById(R.id.tool_bar);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        replaceFragment(SearchFragment.newInstance());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
 }
